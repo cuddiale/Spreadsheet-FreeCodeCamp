2d33f32ffe2588d9ebe0e2eff8747541a0ff4eac
cuddiale <alessiocuddretto@hotmail.com>
2024-06-06 17:37:40 +0200

    Update

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const infixToFunction = {
 };
 
 const infixEval = (str, regex) =>
-  str.replace(regex, (_match, arg1, operator, arg2) => {});
+  str.replace(regex, (_match, arg1, operator, arg2) =>
+    infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
+  );
+
+const highPrecedence = (str) => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
+};
 
 const isEven = (num) => num % 2 === 0;
 const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
@@ -26,6 +34,8 @@ const spreadsheetFunctions = {
   average,
   median,
 };
+
+const applyFunction = (str) => {};
 
 const range = (start, end) =>
   Array(end - start + 1)
