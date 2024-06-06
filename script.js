201ad7b159e6583deb45367ac3dafc93bcad0b6b
cuddiale <alessiocuddretto@hotmail.com>
2024-06-06 12:05:15 +0200

    Update

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  );
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -8,5 +14,17 @@ window.onload = () => {
     label.className = "label";
     label.textContent = name;
     container.appendChild(label);
-  }
-}
+  };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
+    });
+  });
+};
